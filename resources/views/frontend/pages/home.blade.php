689e7475cf732fb33bf3626a0c0725a476e2fc0e
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-05-06 06:49:37 +0300

    Update homepage to display latest posts dynamically
    Modified the homepage to replace "Most Viewed" with "Latest Posts," fetching the three most recent posts. Adjusted the controller logic to sort posts by latest and simplified tab structure by removing redundant items.

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -312,7 +312,7 @@
                     <ul class="nav nav-pills nav-justified">
                         <li class="nav-item">
                             <a class="nav-link active" data-toggle="pill" href="#m-viewed"
-                            >Most Viewed</a
+                            >Latest Posts</a
                             >
                         </li>
                         <li class="nav-item">
@@ -320,40 +320,30 @@
                             >Most Read</a
                             >
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#m-recent"
-                            >Most Recent</a
-                            >
-                        </li>
-                    </ul>
 
+                    </ul>
+                    @php
+                        $latest_three_posts =$posts->take(3);
+                    @endphp
                     <div class="tab-content">
+                        {{-- Content Latest News--}}
                         <div id="m-viewed" class="container tab-pane active">
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-5.jpg"/>
+                            @foreach($latest_three_posts as $post)
+
+                                <div class="tn-news">
+                                    <div class="tn-img">
+                                        {{--<img src="{{$post->images->first()->path}}" alt="{{$post->title}}"/>--}}
+
+                                        <img src="{{asset('assets/frontend')}}/img/news-350x223-5.jpg" alt="{{$post->title}}"/>
+                                    </div>
+                                    <div class="tn-title">
+                                        <a href="">{{$post->title}}</a>
+                                    </div>
                                 </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-4.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-3.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
+                            @endforeach
+
                         </div>
+
                         <div id="m-read" class="container tab-pane fade">
                             <div class="tn-news">
                                 <div class="tn-img">
@@ -374,32 +364,6 @@
                             <div class="tn-news">
                                 <div class="tn-img">
                                     <img src="{{asset('assets/frontend')}}/img/news-350x223-3.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="m-recent" class="container tab-pane fade">
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-4.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-5.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-1.jpg"/>
                                 </div>
                                 <div class="tn-title">
                                     <a href="">Lorem ipsum dolor sit amet</a>
@@ -430,9 +394,9 @@
                                 </div>
                             </div>
                         @endforeach
-                      <div class="d-flex align-items-center justify-content-center">
-                          {{$posts->links()}}
-                      </div>
+                        <div class="d-flex align-items-center justify-content-center">
+                            {{$posts->links()}}
+                        </div>
                     </div>
                 </div>
 
