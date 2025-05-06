cfc38cd1591ac5e8d94fcb88f7f412e87a3ef37c
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-05-06 08:01:15 +0300

    Update home page tabs to display dynamic post-data
    Replaced static content for "Oldest" and "Popular" news tabs with dynamically retrieved posts. Added `oldest_posts` and `popular_posts` data to the HomeController to support this change. Adjusted tab navigation and content rendering using the updated data structure.

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,8 +11,18 @@ class HomeController extends Controller
     public function index()
     {
         $posts = Post::with('images')->latest()->paginate(9);
-        $greatest_posts_views = Post::orderBy('num_of_views', 'desc')->limit(3)->get();
 
-        return view('frontend.pages.home', compact('posts', 'greatest_posts_views'));
+        $greatest_posts_views = Post::orderBy('num_of_views', 'desc')
+            ->limit(3)
+            ->get();
+
+        $oldest_posts = Post::oldest()->take(3)->get();
+
+        $popular_posts = Post::withCount('comments')
+            ->orderBy('comments_count', 'desc')
+            ->limit(3)
+            ->get();
+
+        return view('frontend.pages.home', compact('posts', 'greatest_posts_views', 'oldest_posts', 'popular_posts'));
     }
 }
--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -210,100 +210,39 @@
                 <div class="col-md-6">
                     <ul class="nav nav-pills nav-justified">
                         <li class="nav-item">
-                            <a class="nav-link active" data-toggle="pill" href="#featured"
-                            >Featured News</a
-                            >
+                            <a class="nav-link active" data-toggle="pill" href="#featured">Oldest News</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#popular"
-                            >Popular News</a
-                            >
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" data-toggle="pill" href="#latest"
-                            >Latest News</a
-                            >
+                            <a class="nav-link" data-toggle="pill" href="#popular">Popular News</a>
                         </li>
                     </ul>
 
                     <div class="tab-content">
                         <div id="featured" class="container tab-pane active">
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-1.jpg"/>
+                            @foreach($oldest_posts as $post)
+                                <div class="tn-news">
+                                    <div class="tn-img">
+                                        <img src="{{asset('assets/frontend')}}/img/news-350x223-1.jpg" alt="{{$post->title}}"/>
+                                    </div>
+                                    <div class="tn-title">
+                                        <a href="">{{$post->title}}</a>
+                                    </div>
                                 </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-2.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-3.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
+                            @endforeach
+
                         </div>
                         <div id="popular" class="container tab-pane fade">
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-4.jpg"/>
+                            @foreach($popular_posts as $post)
+                                <div class="tn-news">
+                                    <div class="tn-img">
+                                        <img src="{{asset('assets/frontend')}}/img/news-350x223-4.jpg" alt="{{$post->title}}"/>
+                                    </div>
+                                    <div class="tn-title">
+                                        <a href="">{{$post->title}} ({{$post->comments_count }} <i class="fa-solid fa-comment"></i>)</a>
+                                    </div>
                                 </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-5.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-1.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="latest" class="container tab-pane fade">
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-2.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-3.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
-                            <div class="tn-news">
-                                <div class="tn-img">
-                                    <img src="{{asset('assets/frontend')}}/img/news-350x223-4.jpg"/>
-                                </div>
-                                <div class="tn-title">
-                                    <a href="">Lorem ipsum dolor sit amet</a>
-                                </div>
-                            </div>
+                            @endforeach
+
                         </div>
                     </div>
                 </div>
@@ -316,7 +255,6 @@
                         <li class="nav-item">
                             <a class="nav-link" data-toggle="pill" href="#m-read">Most Read</a>
                         </li>
-
                     </ul>
                     @php
                         $latest_three_posts =$posts->take(3);
