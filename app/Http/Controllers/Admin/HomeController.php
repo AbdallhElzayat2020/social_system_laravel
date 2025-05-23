f6d8e8af09e37d973705bbca6ab710257b9b0048
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-05-23 08:37:28 +0300

    Add charts to the admin dashboard home page
    Integrated Laravel Charts package to display "Posts by months" and "Users by months" charts on the admin dashboard. Updated the HomeController, views, and dependencies to support the new chart functionality.

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,12 +4,44 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
 class HomeController extends Controller
 {
     //
     public function index()
     {
-        return view('dashboard.pages.home');
+
+        $chart_options = [
+            'chart_title' => 'Posts by months',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\Post',
+
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'month',
+            'chart_type' => 'line',
+
+            'filter_field' => 'created_at',
+            'filter_days' => 3600,
+        ];
+        $post_chart_options = new LaravelChart($chart_options);
+
+
+        $chart_options = [
+            'chart_title' => 'Users by months',
+            'report_type' => 'group_by_date',
+            'model' => 'App\Models\User',
+
+            'group_by_field' => 'created_at',
+            'group_by_period' => 'month',
+            'chart_type' => 'bar',
+
+            'filter_field' => 'created_at',
+            'filter_days' => 3600,
+        ];
+
+        $users_chart_options = new LaravelChart($chart_options);
+
+        return view('dashboard.pages.home', compact('post_chart_options', 'users_chart_options'));
     }
 }
